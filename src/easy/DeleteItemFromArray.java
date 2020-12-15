5e21c0069ec335bd4c8a054a023a385fda9d8b7f
o.malikov <o.malikov@p-s.kz>
2020-12-16 00:40:58 +0600

    stupid algorithm deleting from array

---
+++ src/easy/DeleteItemFromArray.java
@@ -0,0 +1,21 @@
+package easy;
+
+public class DeleteItemFromArray {
+
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 2, 3};
+        int val = 3;
+        System.out.println("removeElement(nums,val) = " + removeElement(nums, val));
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+}
--- src/easy/RichestCustomerWealth.java
+++ src/easy/RichestCustomerWealth.java
@@ -18,4 +18,10 @@ public class RichestCustomerWealth {
         return Collections.max(wealth);
 
     }
+
+
+
+
+
+
 }
